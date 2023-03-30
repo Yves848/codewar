65bdb4a03082b1516b1697222f8e6d428e24e8dd
yves848 <yves.godart@gmail.com>
2023-03-30 19:54:21 +0200

    code

--- powershell/compsame/test.ps1
+++ powershell/compsame/test.ps1
@@ -1,16 +1,26 @@
 function comp($a1, $a2)
 {
+  $r = $false
   if ((-not $a1) -or (-not $a2)) {
-    return $false
+    $r=$false
   }
   [System.Collections.ArrayList]$arr = $a2 | ForEach-Object {$_}
   foreach($a in $a1) {
     $po = [System.Math]::Pow($a,2)
-    if (-not $arr.Contains($po)) {
-      return $false
+    $index = $arr.IndexOf($po)
+    if ($index -eq -1) {
+      #Write-Host "$($a)^2 not found"
+      $r = $false
+    }
+    else {
+      #Write-Host "$($a)^2 = $($arr[$index])"
+      $r = $true
     }
   }
-  return $true
+  Write-Host "a1 : $($a1)"
+  Write-Host "a2 : $($a2)"
+  Write-Host "result : $($r)"
+  return $r
 }
 
 function test {
@@ -18,6 +28,7 @@ function test {
   for ($i = 0; $i -lt $lines.Count; $i+=2) {
     [array]$a1 = ([string]$lines[$i]).Split(' ') | ForEach-Object {$_}
     [array]$a2 = ([string]$lines[$i+1]).Split(' ') | ForEach-Object {$_}
+    Write-Host "#####-------#####"
     comp $a1 $a2
 
   }
